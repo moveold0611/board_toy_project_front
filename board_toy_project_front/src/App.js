84c04051f998e9b9dd6485bc21973ae1590c5d76
moveold0611 <b44sup97045@gmail.com>
2024-01-26 23:28:45 +0900

    Revert "초기화면구현"
    This reverts commit fb55e47de876c9298e34a40dde70c4f77c444b5d.

--- board_toy_project_front/src/App.js
+++ board_toy_project_front/src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
-import { Route, Routes } from 'react-router';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      실행
-      <Routes>
-        <Route/>
-        <Route/>
-        <Route/>
-        <Route/>
-      </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
